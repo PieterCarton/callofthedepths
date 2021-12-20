0e45c2a2c939fc1fc920e4a241d9e3c67a9546b6
Pieter Carton <pieterc.n@hotmail.com>
2021-12-20 22:22:19 +0100

    restructure package structure

--- src/main/java/callofthedepths/common/setup/CallOfTheDepths.java
+++ src/main/java/callofthedepths/common/setup/CallOfTheDepths.java
@@ -1,30 +1,27 @@
-package com.example.examplemod;
+package callofthedepths.common.setup;
 
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.stream.Collectors;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod("callofthedepths")
-public class ExampleMod
-{
-    // Directly reference a log4j logger.
+public class CallOfTheDepths {
     private static final Logger LOGGER = LogManager.getLogger();
 
-    public ExampleMod() {
+    public CallOfTheDepths() {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         // Register the enqueueIMC method for modloading
